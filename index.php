983b7c1099a6f05f00a4766fe1000660d75a6a51
GerardoITA <gerardo.giusti@tiscali.it>
2023-01-11 04:33:08 +0100

    esercizio finito

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head lang="<?php echo $str_language; ?>" xml:lang="<?php echo $str_language; ?>">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>MAMP PRO</title>
+<style type="text/css">
+    .container {
+        margin: 50px auto;
+        display: flex; 
+        gap: 10px 
+
+    }
+    .card {
+        width: 200px;
+        height: 400px;
+        background-color: yellow;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+    
+    }
+    h1,h2,h3,h4,h5,h6 {
+        text-align: center;
+    }
+</style>
 <?php
     $hotels = [
 
@@ -41,3 +69,30 @@
 
     
 ?>
+
+</head>
+
+<body>
+    <div class="container">
+        <?php 
+            foreach($hotels as $hotel) {
+
+                $nomeHotel = $hotel['name'];
+                $descrizioneHotel = $hotel['description'];
+                $parcheggioHotel = $hotel['parking'];
+                $votoHotel = $hotel['vote'];
+                $distanzaHotel = $hotel['distance_to_center'];
+
+                echo '<div class="card"> <h1>' . $nomeHotel . '</h1> <h3>' . $descrizioneHotel . '</h3> <h4> Parcheggio: ' . ($parcheggioHotel ? 'Si' : 'No') . '</h4> <h5>' . $votoHotel . '</h5> <h6> Distanza da centro: ' . $distanzaHotel . 'km</h6> </div>';
+        
+            
+            }
+        ?>
+    </div>
+    <p>
+    
+   
+    </p>
+    
+</body>
+</html>
